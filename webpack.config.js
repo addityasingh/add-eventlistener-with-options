efb2bd8f2167fb8cdc2005bd059a47d9458e8eb2
Aditya Pratap Singh <adisinghrajput@gmail.com>
2016-10-19 23:51:05 +0200

    Publish the package to the lib root to be imported as node module

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, 'lib'),
+        path: path.join(__dirname),
         filename: 'index.js',
         libraryTarget: 'umd',
         library: 'add-eventlistener-with-options'  
